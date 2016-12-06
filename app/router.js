63d1baa062f27fe8b4b11ff8f6593a765d9f2bd4
Bastien Munier <munier.bastien@gmail.com>
2016-12-06 22:39:55 +0100

    First commit: main page, menu and routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('stopwatch');
+  this.route('tabata');
+  this.route('amrap');
+  this.route('emom');
 });
 
 export default Router;
